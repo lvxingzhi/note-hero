347c1bf20990dddc29684073b059f58148acd8a6
lvxz5 <lvxz5@ziroom.com>
2016-10-10 11:35:24 +0800

    init file

---
+++ hero-base/src/main/java/com/note/hero/base/util/DateUtil.java
@@ -0,0 +1,18 @@
+package com.note.hero.base.util;
+
+/**
+ * <p>日期处理工具 </p>
+ *
+ * <PRE>
+ * <BR>	修改记录
+ * <BR>-----------------------------------------------
+ * <BR>	修改日期			修改人			修改内容
+ * </PRE>
+ *
+ * @author lvxz5
+ * @version 1.0
+ * @date 2016/10/10
+ * @since 1.0
+ */
+public class DateUtil {
+}
